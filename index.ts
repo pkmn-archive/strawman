aab5441cf36a296c203f6c98d70253e2fcb98fa7
Kirk Scheibelhut <kjs@scheibo.com>
2019-10-20 12:33:30 -0700

    Min impl

--- index.ts
+++ index.ts
@@ -1,50 +1,27 @@
 type ID = '' | string & {__isID: true};
 declare function toID(s: string): ID;
 
-type primitive = string | number | boolean | undefined | null;
+type primitive = string | number | boolean | undefined | null | Function | ID;
 type DeepReadonly<T> = T extends primitive ? T : DeepReadonlyObject<T>;
 type DeepReadonlyObject<T> = {
   readonly [P in keyof T]: DeepReadonly<T[P]>
 }
 
-type Nullable<T> = {
+type Nullable<T> = T extends primitive ? T : NullableObject<T>;
+type NullableObject<T> = {
   [P in keyof T]: Nullable<T[P]> | null;
 }
 
-// ---------- conditions.ts ----------
-
-type WeatherName =
-  | 'Sand'
-  | 'Sun'
-  | 'Rain'
-  | 'Hail'
-  | 'Harsh Sunshine'
-  | 'Heavy Rain'
-  | 'Strong Winds';
-
 // ---------- data.ts ----------
 
-type DataKind = 'Ability' | 'Move' | 'Item' | 'Species';
+type DataKind = 'Item' | 'Species';
 
-
-interface Descriptions {
-  desc: string;
-  shortDesc: string;
-}
-interface JSONDescription extends Nullable<{
-  items?: {[id: string]: Descriptions};
-  moves?: {[id: string]: Descriptions};
-  species?: {[id: string]: Descriptions};
-}> {}
-
-interface JSONData extends Nullable<Omit<Data, 'id' | 'kind' | 'gen' | 'desc' | 'shortDesc'>> {}
+interface JSONData extends Nullable<Omit<Data, 'id' | 'kind' | 'gen'>> {}
 interface Data {
   id: ID;
   kind: DataKind;
-  name: string; // TODO store as key 
+  name: string;
   gen: Generation;
-  desc: string;
-  shortDesc: string;
 }
 
 // ---------- items.ts ----------
@@ -58,83 +35,179 @@ interface Item extends DeepReadonly<Data & CommonItem<Species> & {
   kind: 'Item';
 }> {}
 
-class Items {
-  get(id: ID): Item | undefined {
-    return undefined;
-  }
-  [Symbol.iterator](): IterableIterator<Item> {
-
-  }
-}
-
-// ---------- moves.ts ----------
-
-interface CommonMove<MoveT> extends DeepReadonly<{
-  basePower?: number;
-  pp: number;
-  noMetronome: MoveT[];
-}> {}
-
-interface JSONMove extends Nullable<Data & CommonMove<ID>> {}
-interface Move extends DeepReadonly<Data & CommonMove<Move> & {
-  kind: 'Move';
-  noMetronome: Move[];
-}> {}
-
-class Moves {
-  get(id: ID): Move | undefined {
-    return undefined;
-  }
-  [Symbol.iterator](): IterableIterator<Move> {
-
-  }
-}
-
 // ---------- species.ts ----------
 
-interface CommonSpecies<ItemT, MoveT, SpeciesT> {
+interface CommonSpecies<ItemT,SpeciesT> {
   num: number;
   evos?: SpeciesT[];
   requiredItems?: ItemT[];
-  requiredMove?: MoveT;
 }
 
-interface JSONSpecies extends Nullable<JSONData & CommonSpecies<ID, ID, ID>> {}
-interface Species extends DeepReadonly<Data & CommonSpecies<Item, Move, Species> & {
-  type: 'Species';
+interface JSONSpecies extends Nullable<JSONData & CommonSpecies<ID, ID>> {}
+interface Species extends DeepReadonly<Data & CommonSpecies<Item, Species> & {
+  kind: 'Species';
   requiredItems: Item[];
 }> {}
 
-class SpeciesSummaries {
-  get(id: ID): Species | undefined {
-    return undefined;
-  }
-  [Symbol.iterator](): IterableIterator<Species> {
-
-  }
-}
 
 // ---------- gen.ts ----------
 
 type GenerationNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7;
-// NOTE: CAP is not currently considered a mod but should be...
-type Mod = 'LGPE' | 'Stadium' | 'CAP' | 'SSB' | 'Next' | 'PiC' | 'Mix And Mega' | 'VGC 17';
-
-type Language =
-  'de' | 'en' | 'es' | 'fr' | 'hi' | 'it' | 'ja' |
-  'nl' | 'pl' | 'pt' | 'ru' | 'tr' | 'tw' | 'zh';
 
 interface Generation extends DeepReadonly<{
   num: GenerationNumber;
-  mod?: Mod;
-
   items: Items;
-  moves: Moves;
   species: SpeciesSummaries;
 }> {}
 
-class Generations {
-  static async get(generation: GenerationNumber, mod?: Mod, lang: Language = 'en'): Promise<Generation> {
-    
+/////////// IMPL ///////////
+
+class GenerationImpl implements Generation {
+  num: GenerationNumber;
+  items: Items;
+  species: SpeciesSummaries;
+
+  constructor(num: GenerationNumber, items: Items, species: SpeciesSummaries) {
+    this.num = num;
+    this.items = items;
+    this.species = species;
   }
 }
+
+class Generations {
+  // not the correct signature, but for this example with one gen, no mods or langs and no loading its sufficient
+  static get(): Generation {
+    // FIXME: generations API needs items/species APIs, but species/items APIs needs generations API to resolve references 
+  }
+}
+// NB: obviously would use caching etc
+class Items  {
+  private readonly gen: Generation;
+  private readonly json: {[id: string]: JSONItem} = {
+    "charizarditey": {
+        "name": "Charizardite Y",
+        "megaEvolves": "charizard" as ID
+    },
+    "charizarditex": {
+        "name": "Charizardite X",
+        "megaEvolves": "charizard" as ID
+    },
+    "leftovers": {
+        "name": "Leftovers"
+    }
+  };
+
+  constructor(gen: Generation) {
+    this.gen = gen;
+  }
+
+  get(id: ID): Item | undefined {
+    const data = this.json[id];
+    return data ? new ItemImpl(this.gen, data) : undefined;
+  }
+}
+
+class DataImpl implements Data {
+  gen: Generation;
+  kind: DataKind;
+  name: string;
+  id: ID;
+
+  constructor(gen: Generation, kind: DataKind, name: string) {
+    this.id = toID(name);
+    this.kind = kind;
+    this.gen = gen;
+  }
+}
+
+class ItemImpl extends DataImpl implements Item {
+  private readonly data: JSONItem;
+
+  constructor(gen: Generation, data: JSONItem) {
+    super(gen, 'Item', data.name);
+    this.data = data;
+  }
+
+  // obviously would use caching etc
+  get megaEvolves() {
+    if (!this.data.megaEvolves) return undefined;
+    return this.gen.species.get(this.data.megaEvolves);
+  }
+}
+
+class SpeciesSummaries {
+  private readonly gen: Generation;
+  private readonly json:{[id: string]: JSONSpecies} = {
+    "charmander": {
+        "num": 4,
+        "name": "Charmander",
+        "evos": ["charmeleon"] as ID[],
+    },
+    "charmeleon": {
+        "num": 5,
+        "name": "Charmeleon",
+        "evos": ["charizard"] as ID[],
+        "requiredItems": ["leftovers"] as ID[]
+    },
+    "charizard": {
+        "num": 6,
+        "name": "Charizard",
+        "requiredItems": ["charizarditex", "charizarditey"] as ID[]
+  
+    },
+    "marshadow": {
+        "num": 999,
+        "name": "Marshadow"
+    }
+  };
+
+  constructor(gen: Generation) {
+    this.gen = gen;
+  }
+
+  get(id: ID): Species | undefined {
+    const data = this.json[id];
+    return data ? new SpeciesImpl(gen, data) : undefined;
+  }
+}
+
+class SpeciesImpl extends DataImpl implements Species {
+  num: number;
+  data: JSONSpecies;
+
+  constructor(gen: Generation, data: JSONSpecies) {
+    super(gen, 'Species', data.name);
+    this.num = data.num;  
+  }
+
+  get evos() {
+    if (!this.data.evos) return undefined;
+    return this.data.evos.map(evo => this.gen.species.get(evo));
+  }
+
+  get requiredItems() {
+    if (!this.data.requiredItems) return undefined;
+    return this.data.requiredItems.map(item => this.gen.items.get(item));
+  }
+}
+
+/////////// EXTENSION ///////////
+
+interface SpeciesDetails {
+  color: string;
+}
+
+const detailsJSON = {
+  "charmander": {
+      "color": "Red"
+  },
+  "charmeleon": {
+      "color": "Red"
+  },
+  "charizard": {
+      "color": "Red"
+  },
+  "marshadow": {
+      "color": "Black"
+  }
+};
